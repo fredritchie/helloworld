47469e751026bfe2fbb648228b2f5d639b93331e
unknown <fredrickritchie@gmail.com>
2023-06-05 15:21:55 +0530

    Initial commit

--- helloworld/src/main/java/com/intern/helloworld/HelloWorldDemo.java
+++ helloworld/src/main/java/com/intern/helloworld/HelloWorldDemo.java
@@ -10,3 +10,43 @@ public class HelloWorldDemo {
         return "Hello World";
     }
 }
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@SpringBootTest
+@Testcontainers
+class HelloWorldApplicationTests {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest");
+
+    @Autowired
+    private YourRepository yourRepository;
+    //replace YourRepository with your actual repositor
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @Test
+    void testDatabaseConnection() {
+        // Perform database tests using yourRepository and entityManager
+    }
+}
